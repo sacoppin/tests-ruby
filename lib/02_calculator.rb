50d0b6799164471e1e230ebc19ace03400c5d2cf
Sarah Coppin <sacoppin@hotmail.com>
2022-01-24 22:44:43 +0100

    commit2

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -1,20 +1,74 @@
-#Addition
-    def add(n1,n2)
-    n1.to_i + n2.to_i
-  end
-  #Soustraction
-     def substract(n1,n2)
-    n1.to_i - n2.to_i
-  end
-# Réalise la somme des éléments d'un array
-  def sum(array) # See inject method >>> https://medium.com/@terrancekoar/inject-method-explained-ed531eff9af8
-    array.inject(0) { |result, element| result + element }
-  end
+
+#Addition de 2 nombres positifs
+
+def add(n1,n2)
+  n1.to_i + n2.to_i
+end
+
+
+#Soustraction entre 2 nombres 
+def subtract(n1,n2)
+	n1 = n1.to_i
+	n2 = n2.to_i
+	
+	return n1 - n2
+	
+end
+
+
+#Somme des éléments d'un array
+def sum(array)
+
+	return array.sum
+
+end
+
 # Méthode permettant de réaliser le produit de 2 nombres
-def multiply(n1, n2)
-    n1.to_i * n2.to_i
+
+def multiply(n1,n2)
+	n1 = n1.to_i
+	n2 = n2.to_i
+
+	return n1*n2
+	
+end
+
+
+#Methode permettant de mettre un nombre chiffre_1 à la 
+
+def power(chiffre_1,chiffre_2)
+	chiffre_1 = chiffre_1.to_i
+	chiffre_2 = chiffre_2.to_i
+
+	return chiffre_1**chiffre_2
+	
+end
+
+# Methode permettant de calculer les factoriels 5! = 5*4*3*2*1 par exemple
+
+
+def factorial(val)
+
+  if val < 0
+	return nil
+
+  elsif val == 0
+
+    return 1
+    
+  else
+
+  	result = 1
+
+  	while val>0 
+  		result = result *val
+
+  		val -= 1
+   
+  	end
+
   end
-# Facto # Calcul de la factorielle
-def power(n1, n2)
-    n1.to_i ** n2.to_i
-  end
+
+  return result
+
+end
--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,26 +1,28 @@
-# Hint: 'def who_is_bigger(a, b, c)'
 def who_is_bigger(a, b, c)
-    array = [a, b, c]
-    if a == nil || b == nil || c == nil
-      "nil detected"
-    elsif array.max == a
-      "a is bigger"
-    elsif array.max == b
-      "b is bigger"
-    else
-      "c is bigger"
-    end
+  array = [a, b, c]
+  if a == nil || b == nil || c == nil
+    "nil detected"
+  elsif array.max == a
+    "a is bigger"
+  elsif array.max == b
+    "b is bigger"
+  else
+    "c is bigger"
   end
+end
 
+def reverse_upcase_noLTA(string)
+  string.reverse.upcase.gsub(/[ltaLTA]/, '')
+end
 
-# Reverse, upcase then removes all L, T and A.
-  # Hint: google ruby string
-  def
-    crazy = {Tries this at Home, Kids}
+def array_42(array)
+  if array.include?(42)
+    true
+  else 
+    false
+  end
+end
 
-    crazy.reverse= crazyreverse
-    crazyreverse.upcase=crazy2
-    finalcrazy= crazy2.delete("LTA")
-   
-  "Ponies loves carrots"
-  "{qwertyuiopasdfghjkl;zxcvbn}"
+def magic_array(list)
+  list.flatten.sort.map{|x| x.to_i * 2}.delete_if{|x| x%3 == 0}.uniq.sort
+end
